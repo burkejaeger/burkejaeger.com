3a2148f5d0a51651ec46d9ba875910aafa6d0109
burkejaeger <81589723+burkejaeger@users.noreply.github.com>
2022-05-10 15:05:48 -0400

    updated theme

--- src/components/app.js
+++ src/components/app.js
@@ -2,18 +2,55 @@ import React from 'react';
 import {
   BrowserRouter as Router, Route, Routes, NavLink, useParams,
 } from 'react-router-dom';
+import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
 import Counter from './counter';
 import Controls from './control';
 
-function About(props) {
-  return <div> All there is to know about me </div>;
-}
-function Welcome(props) {
+function Experiences(props) {
   return (
     <div>
-      <Counter />
-      <Controls />
-      <div>Welcome</div>
+      <div> All there is to know about me </div>
+      <div>
+        <Card>
+          <div className="experience-left-column">
+            <div className="experience-title">BMW Group</div>
+            <div className="bmwi4" />
+          </div>
+        </Card>
+        <Card>
+
+          RWJBarnabas Health          <Button> View the LiveStream </Button>
+        </Card>
+      </div>
+    </div>
+  );
+}
+function Projects(props) {
+  return (
+    <div>
+      <div className="logo" />
+      <div> Burke J. Jaeger </div>
+      <div> Sotware Developer </div>
+      <div> Research Assistant </div>
+      <div> Dartmouth Undergraduate </div>
+      <Card>
+        Healing Together
+        <Button> View the LiveStream </Button>
+      </Card>
+      <Card>
+        Healing Together
+        <Button> View the LiveStream </Button>
+      </Card>
+      <Card>
+        Healing Together
+        <Button> View the LiveStream </Button>
+      </Card>
+      <div>
+        <Counter />
+        <Controls />
+        <div>Welcome</div>
+      </div>
     </div>
   );
 }
@@ -24,8 +61,8 @@ function App(props) {
       <div>
         <Nav />
         <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Projects />} />
+          <Route path="/experiences" element={<Experiences />} />
           <Route path="/test/:id" element={<Test />} />
           <Route path="*" element={<FallBack />} />
         </Routes>
@@ -38,8 +75,8 @@ function Nav(props) {
   return (
     <nav>
       <ul>
-        <li><NavLink to="/" exact="true">Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/" exact="true">Projects</NavLink></li>
+        <li><NavLink to="/experiences">Experiences</NavLink></li>
         <li><NavLink to="/test/id1">test id1</NavLink></li>
         <li><NavLink to="/test/id2">test id2</NavLink></li>
       </ul>
